07089fe71e1d5562eb6953417a48011b7f17b9af
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-05-14 13:05:19 -0600

    Initial commit

---
+++ octoprint_snapmaker_extended/static/js/snapmaker_extended.js
@@ -0,0 +1,29 @@
+/*
+ * View model for SnapMaker Extended
+ *
+ * Author: Ben Drummond
+ * License: MIT
+ */
+$(function() {
+    function Snapmaker_extendedViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: Snapmaker_extendedViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_snapmaker_extended, #tab_plugin_snapmaker_extended, ...
+        elements: [ /* ... */ ]
+    });
+});
