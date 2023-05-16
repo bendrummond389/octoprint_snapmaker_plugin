f4b000b4c39b507356400a24f6f8b0695c571d79
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-05-16 16:09:46 -0600

    Improve SnapmakerExtendedPlugin Code Quality
    Removed unnecessary comments to increase code readability.
    Reorganized all octoprint.plugin.BlueprintPlugin.route decorators into a separate section for better visibility and code structure.
    Grouped related functionalities together, specifically settings and assets functions for more logical arrangement.
    Ensured consistent use of single quotes across the code.
    Removed deprecated get_api_commands and on_api_command methods, using BlueprintPlugin.route decorators instead.
    Optimized get_update_information method by using f-string formatting for pip URL creation."

--- octoprint_snapmaker_extended/__init__.py
+++ octoprint_snapmaker_extended/__init__.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 from __future__ import absolute_import
 import octoprint.plugin
-from octoprint.events import Events
 from flask import jsonify, request
 
 
@@ -12,9 +11,7 @@ class SnapmakerExtendedPlugin(
     octoprint.plugin.BlueprintPlugin,
 ):
     def get_settings_defaults(self):
-        return {
-            # put your plugin's default settings here
-        }
+        return {}
 
     def get_assets(self):
         return {
@@ -22,16 +19,6 @@ class SnapmakerExtendedPlugin(
             "css": ["css/snapmaker_extended.css"],
             "less": ["less/snapmaker_extended.less"],
         }
-
-    # def get_api_commands(self):
-    #     return {
-    #         "autolevel": []
-    #     }
-
-    # def on_api_command(self, command, data):
-    #     if command == "autolevel":
-    #         self._printer.commands("G1029 A")
-    #         return jsonify(success=True)
 
     def get_update_information(self):
         return {
@@ -42,7 +29,7 @@ class SnapmakerExtendedPlugin(
                 "user": "bendrummond389",
                 "repo": "octoprint_snapmaker_plugin",
                 "current": self._plugin_version,
-                "pip": "https://github.com/bendrummond389/octoprint_snapmaker_plugin/archive/{target_version}.zip",
+                "pip": f"https://github.com/bendrummond389/octoprint_snapmaker_plugin/archive/{self._plugin_version}.zip",
             }
         }
 
@@ -58,8 +45,8 @@ class SnapmakerExtendedPlugin(
 
     @octoprint.plugin.BlueprintPlugin.route("/engraveTestLines", methods=["POST"])
     def engrave_test_lines(self):
-        laser_power = 255  # Adjust as necessary
-        commands = ["G92 X0 Y0"]  # Move to start position
+        laser_power = 255
+        commands = ["G92 X0 Y0"]
 
         for line in range(20):
             z = 3 + (line * 0.5)
@@ -75,16 +62,49 @@ class SnapmakerExtendedPlugin(
             )
 
         self._printer.commands(commands)
-        return jsonify(success=True)  # return response
+        return jsonify(success=True)
+
+    @octoprint.plugin.BlueprintPlugin.route("/engraveTestBoxes", methods=["POST"])
+    def engrave_test_boxes(self):
+        num_boxes = 20
+        box_size = 1
+        max_power = 255
+        min_power = max_power * 0.1
+        max_feed_rate = 5000
+        min_feed_rate = 1000
+        commands = ["G92 X0 Y0"]
+
+        for i in range(num_boxes):
+            for j in range(num_boxes):
+                x = i * box_size
+                y = j * box_size
+                power = min_power + ((max_power - min_power) / (num_boxes - 1) * i)
+                feed_rate = min_feed_rate + (
+                    (max_feed_rate - min_feed_rate) / (num_boxes - 1) * j
+                )
+
+                commands.extend(
+                    [
+                        f"G1 Z0",
+                        f"G1 X{x} Y{y} F{feed_rate:.0f}",
+                        f"M3 P{power:.0f}",
+                        f"G1 X{x+box_size} Y{y} F{feed_rate:.0f}",
+                        f"G1 X{x+box_size} Y{y+box_size} F{feed_rate:.0f}",
+                        f"G1 X{x} Y{y+box_size} F{feed_rate:.0f}",
+                        f"G1 X{x} Y{y} F{feed_rate:.0f}",
+                        "M5",
+                    ]
+                )
+
+        self._printer.commands(commands)
+        return jsonify(success=True)
 
     @octoprint.plugin.BlueprintPlugin.route("/setFocusedZOffset", methods=["POST"])
     def set_focused_z_offset(self):
         selected_line = request.json.get("selectedLine")
-        new_z_offset = 1 + (
-            float(selected_line) * 0.5
-        )  # convert selected_line to float
+        new_z_offset = 1 + (float(selected_line) * 0.5)
         self._printer.commands([f"G0 Z{new_z_offset}", "G92 Z0"])
-        return jsonify(success=True)  # return response
+        return jsonify(success=True)
 
 
 __plugin_name__ = "Snapmaker_extended Plugin"
