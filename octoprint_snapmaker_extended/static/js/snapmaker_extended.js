e1025d422b6b313e544db5dd5150fa0858a51737
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-05-19 18:33:48 -0600

    Added distance changer function

--- octoprint_snapmaker_extended/static/js/snapmaker_extended.js
+++ octoprint_snapmaker_extended/static/js/snapmaker_extended.js
@@ -8,25 +8,28 @@ $(function () {
     function SnapmakerExtendedViewModel(parameters) {
         var self = this;
 
+        // This is your currently selected line, initialized to 0
         self.selectedLine = ko.observable(0);
 
+        // Obtain the controlViewModel from the parameters
         var controlViewModel = parameters[0];
 
-        // Access functions from the ControlViewModel
+        // Import functions from the ControlViewModel
         self.isOperational = controlViewModel.isOperational;
         self.isPrinting = controlViewModel.isPrinting;
-        self.sendJogCommand = controlViewModel.sendJogCommand;
         self.stripDistanceDecimal = controlViewModel.stripDistanceDecimal;
 
+        // These are the observables for your distances
         self.distances = ko.observableArray([0.1, 1, 10, 100]);
         self.distance = ko.observable(10);
 
-        // TODO: Implement your plugin's view model here.
+        // Debugging: Log initial values
+        console.log('Initial values:', self.distance(), self.distances());
+
+        // This function performs auto-leveling by sending a POST request to the server
         self.performAutoLevel = function () {
             $.ajax({
-                url:
-                    window.location.origin +
-                    "/plugin/snapmaker_extended/autolevel",
+                url: window.location.origin + "/plugin/snapmaker_extended/autolevel",
                 type: "POST",
                 dataType: "json",
                 data: JSON.stringify({
@@ -39,11 +42,10 @@ $(function () {
             });
         };
 
+        // This function sets the Z offset by sending a POST request to the server
         self.setZOffset = function () {
             $.ajax({
-                url:
-                    window.location.origin +
-                    "/plugin/snapmaker_extended/setZOffset",
+                url: window.location.origin + "/plugin/snapmaker_extended/setZOffset",
                 type: "POST",
                 dataType: "json",
                 data: JSON.stringify({}),
@@ -54,11 +56,29 @@ $(function () {
             });
         };
 
+        // This function sends a jog command to the OctoPrint server
+        self.sendJogCommand = function (axis, multiplier, distance) {
+            if (typeof distance === "undefined") distance = self.distance();
+            if (
+                self.settings.printerProfiles.currentProfileData() &&
+                self.settings.printerProfiles.currentProfileData()["axes"] &&
+                self.settings.printerProfiles.currentProfileData()["axes"][axis] &&
+                self.settings.printerProfiles
+                    .currentProfileData()
+                    ["axes"][axis]["inverted"]()
+            ) {
+                multiplier *= -1;
+            }
+
+            var data = {};
+            data[axis] = distance * multiplier;
+            OctoPrint.printer.jog(data);
+        };
+
+        // This function engraves test lines by sending a POST request to the server
         self.engraveTestLines = function () {
             $.ajax({
-                url:
-                    window.location.origin +
-                    "/plugin/snapmaker_extended/engraveTestLines",
+                url: window.location.origin + "/plugin/snapmaker_extended/engraveTestLines",
                 type: "POST",
                 dataType: "json",
                 data: JSON.stringify({}),
@@ -69,11 +89,10 @@ $(function () {
             });
         };
 
+        // This function sets the focused Z offset by sending a POST request to the server
         self.setFocusedZOffset = function (selectedLine) {
             $.ajax({
-                url:
-                    window.location.origin +
-                    "/plugin/snapmaker_extended/setFocusedZOffset",
+                url: window.location.origin + "/plugin/snapmaker_extended/setFocusedZOffset",
                 type: "POST",
                 dataType: "json",
                 data: JSON.stringify({
@@ -87,10 +106,7 @@ $(function () {
         };
     }
 
-    /* view model class, parameters for constructor, container to bind to
-     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
-     * and a full list of the available options.
-     */
+    // Register this view model with OctoPrint
     OCTOPRINT_VIEWMODELS.push({
         construct: SnapmakerExtendedViewModel,
         dependencies: ["controlViewModel"],
